87bfe9f3b8fbb85d4ddc50b9250d27e2cfcc4954
chant <cavram@htl-styr.ac.at>
2022-09-21 18:43:11 +0200

    calculation functions added

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -1,3 +1,4 @@
+
 public class Fraction {
 
     private int dividend = 1;
@@ -25,6 +26,79 @@ public class Fraction {
     public void setDevisor(int devisor) {
         this.devisor = devisor;
     }
+
+    public Fraction add(Fraction other) {
+        int a;
+        int b;
+
+        if (this.devisor != other.devisor) {
+            int x = this.dividend * other.devisor;
+            int y = other.dividend * this.devisor;
+
+            a = x + y;
+            b = this.devisor * other.devisor;
+        } else {
+            a = this.dividend + other.dividend;
+            b = this.devisor;
+        }
+
+        return new Fraction(a, b);
+    }
+
+
+    public Fraction subtract(Fraction other) {
+        int a;
+        int b;
+
+        if (this.devisor != other.devisor) {
+            int x = this.dividend * other.devisor;
+            int y = other.dividend * this.devisor;
+
+            a = x - y;
+            b = this.devisor * other.devisor;
+        } else {
+            a = this.dividend - other.dividend;
+            b = this.devisor;
+        }
+
+        return new Fraction(a, b);
+    }
+
+    public Fraction multiply(Fraction other) {
+        int a = this.dividend * other.dividend;
+        int b = this.devisor * other.devisor;
+
+        return new Fraction(a, b);
+
+    }
+
+    public Fraction divide(Fraction other) {
+        int a = this.dividend * other.devisor;
+        int b = this.devisor * other.dividend;
+
+        return new Fraction(a, b);
+    }
+
+    //auf selben nenner bringen
+    //Ergebis neuer Bruch gekürtzt -> new Fraction
+    public Fraction shorten() {
+        int gcd = 1; //fängt bei 1 an
+
+        //i ist der teiler, prueft so lange bis zur kleinsten zahl vorhanden
+        for (int i = 1; i <= this.dividend && i <= this.devisor; i++) {
+            //prueft ob der  teiler keinen rest zurueck gibt kein komma
+            if (this.dividend % i == 0 && this.devisor % i == 0)
+                //teiler wird refresht
+                gcd = i;
+        }
+
+        //ergebnis durch den neuen teiler
+        int a = this.dividend / gcd;
+        int b = this.devisor / gcd;
+
+        return new Fraction(a, b);
+    }
+
 
     @Override
     public java.lang.String toString() {
--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -39,4 +39,68 @@ class FractionTest {
         Fraction f = new Fraction(17,13);
         Assertions.assertEquals("17/13", f.toString());
     }
+
+    @Test
+    void add() {
+        Fraction f1 = new Fraction(2,4);
+        Fraction f2 = new Fraction(2,2);
+
+        Fraction result = f1.add(f2);
+
+        Assertions.assertEquals(8, result.getDevisor());
+        Assertions.assertEquals(12,result.getDividend());
+    }
+
+    @Test
+    void shorten() {
+
+    }
+
+    @Test
+    void subtract() {
+        Fraction f1 = new Fraction(4,2);
+        Fraction f2 = new Fraction(2,4);
+
+        Fraction result = f1.subtract(f2);
+
+        Assertions.assertEquals(8, result.getDevisor());
+        Assertions.assertEquals(12,result.getDividend());
+    }
+
+    @Test
+    void multiply() {
+        Fraction f1 = new Fraction(4,2);
+        Fraction f2 = new Fraction(2,4);
+
+        Fraction result = f1.multiply(f2);
+
+        Assertions.assertEquals(8, result.getDevisor());
+        Assertions.assertEquals(8,result.getDividend());
+    }
+
+    @Test
+    void divide() {
+
+        Fraction f1 = new Fraction(1,2);
+        Fraction f2 = new Fraction(4,2);
+
+        Fraction result = f1.divide(f2);
+        result.shorten();
+
+        Assertions.assertEquals(8, result.getDevisor());
+        Assertions.assertEquals(2,result.getDividend());
+    }
+
+
+    @Test
+    void testShorten() {
+
+        Fraction f2 = new Fraction(35,80);
+        Fraction result = f2.shorten();
+
+        Assertions.assertEquals(16, result.getDevisor());
+        Assertions.assertEquals(7,result.getDividend());
+
+        Assertions.assertEquals("7/16", result.toString());
+    }
 }
